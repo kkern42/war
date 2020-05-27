06668ec6a68754f0f59e119b2d29c8e85857ba1e
Kevin Kern <kjk9cd@virginia.edu>
2020-05-26 20:22:03 -0400

    may have gotten even uglier

--- src/Display.js
+++ src/Display.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import "./Display.css";
 // function CompareCards(props) {
 //     if (props.user[props.getScore[0]].value > props.machine[props.getScore[0]].value) {
 //         props.setScore([props.getScore[0], props.getScore[1] + 1, props.getScore[2]]);
@@ -46,22 +46,30 @@ function Display(props) {
     }
     return (
         <div>
-            <div style={{ marginLeft: "50vw", marginTop: "20vw" }}>
+            <div style={{ marginLeft: "46vw", marginTop: "20vw" }}>
                 {
                     getCards[0] > 0 && [
-                        <div>
-                            <div>Your card {props.user[getCards[0] - 1].value} of {props.user[getCards[0] - 1].suit} </div>
-                            <div>Machines card {props.machine[getCards[0] - 1].value} of {props.machine[getCards[0] - 1].suit}</div>
-                            <div >Your score: {getCards[1]}</div>
-                            <div> Machine score: {getCards[2]}</div>
+                        <div id="cardParent">
+                            <div className="card">Your card {props.user[getCards[0] - 1].value} of {props.user[getCards[0] - 1].suit} </div>
+                            <div className="card">Machines card {props.machine[getCards[0] - 1].value} of {props.machine[getCards[0] - 1].suit}</div>
                         </div>
 
                     ]
                 }
             </div>
             <button style={{ marginLeft: "50vw", marginTop: "2vw" }} onClick={increment}>Draw</button>
+            <div style={{ marginLeft: "50vw" }}>
+                {
+                    getCards[0] > 0 && [
+                        <div>
+                            <div className="child_div_2">Your score: {getCards[1]}</div>
+                            <div className="child_div_2"> Machine score: {getCards[2]}</div>
+                        </div>
 
-        </div>
+                    ]
+                }
+            </div>
+        </div >
     );
 }
 
